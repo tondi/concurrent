0a4920ba4ae6876105f453c9929d35dd0ad6217b
Mateusz Tondos <mateusz.tondos@sinch.com>
2021-03-20 11:28:14 +0100

    init

---
+++ src/com/agh/IThread.java
@@ -0,0 +1,28 @@
+package com.agh;
+
+// Watek, ktory inkrementuje licznik 100.000 razy
+class IThread extends Thread {
+    Counter counter;
+
+    public IThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while(i < 100000) {
+            while(this.counter.isLocked()) {}
+
+            this.counter.lockCounter();
+            this.counter.inc();
+            this.counter.unlockCounter();
+            i++;
+        }
+    }
+}
+
+// ithread run
+// dthread run
+// ustawienie locka przez watek 1
+//
