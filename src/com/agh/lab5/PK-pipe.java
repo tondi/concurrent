c38b30849d5ba4018bedba4019c110a29edaccd2
tondi <mtondos2@gmail.com>
2021-06-10 00:49:35 +0200

    wip

--- src/com/agh/lab5/PK-pipe.java
+++ src/com/agh/lab5/PK-pipe.java
@@ -1,18 +1,61 @@
-public class PKmon {
-    public static void main(String[] args) {
+package com.agh.lab5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class PKmon {
+    public static long exec(int i) throws InterruptedException {
+        int bufferSize = 5;
+        int iterations = 30;
+        int convertersCount = i;
+
+        List<Thread> threads = new ArrayList<Thread>();
         long start = System.currentTimeMillis();
-        Buffer buffer = new Buffer(bufferSize);
-       …
+
+        Buffer prev = new Buffer(bufferSize);
+        Buffer next = prev; // start with first
+
+        Producer producer = new Producer(next, iterations);
+        threads.add(producer);
+
+        for (int j = 0; j < convertersCount; j++) {
+            prev = next;
+            next = new Buffer(bufferSize);
+            threads.add(new Converter(prev, next, iterations));
+        }
+
+        Consumer consumer = new Consumer(next, iterations);
+        threads.add(consumer);
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+
         long time = System.currentTimeMillis() - start;
-        System.out.println("czas: " + time);
+//        System.out.println("czas: " + time);
+
+        return time;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+//        System.out.println(exec(10));
+
+        for(int i = 100; i <= 100; i++) {
+            System.out.println(i + " " + exec(i));
+        }
     }
 }
-
 class Buffer {
+    private List<Integer> _buf;
     private int _size;
 
     public Buffer(int size) {
         this._size = size;
+        _buf = new ArrayList(size);
     }
 
     public synchronized void put(int i) {
@@ -22,7 +65,9 @@ class Buffer {
             } catch (InterruptedException e) {
             }
         }
-       …
+
+        _buf.add(i);
+        notify();
     }
 
     public synchronized int get() {
@@ -32,58 +77,63 @@ class Buffer {
             } catch (InterruptedException e) {
             }
         }
-       	…
+
+        Integer value = _buf.remove(0);
+        notify();
         return value;
     }
 }
-
 class Consumer extends Thread {
     private Buffer _buf;
+    private int iter;
 
-    public Consumer(Buffer buffer, …) {
+    public Consumer(Buffer buffer, int iterations) {
         this._buf = buffer;
-       …
+        this.iter = iterations;
     }
 
     public void run() {
-        _buf.get();
+        for (int i = 0; i < iter; i++) {
+            _buf.get();
+        }
     }
 }
-
 class Converter extends Thread {
     private Buffer previous;
     private Buffer next;
-       …
+    private int iter;
 
-    public Converter(Buffer previous, Buffer next, …) {
+    public Converter(Buffer previous, Buffer next, int iterations) {
         this.previous = previous;
         this.next = next;
-       …
+        this.iter = iterations;
     }
 
     public void run() {
-       …
-        int tmp = previous.get();
-        try {
-            sleep(10);
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
+        for (int i = 0; i < this.iter; i++) {
+            int tmp = previous.get();
+            tmp += 10;
+            try {
+                sleep(10);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+            next.put(tmp);
         }
-        next.put(tmp);
     }
 }
-
 class Producer extends Thread {
     private Buffer _buf;
     private int iter;
 
     public Producer(Buffer buffer, int iterations) {
         this._buf = buffer;
-       …
+        this.iter = iterations;
     }
 
     public void run() {
-       …
-        _buf.put(i);
+        for (int i = 0; i < iter; i++) {
+            _buf.put(i);
+        }
     }
 }
