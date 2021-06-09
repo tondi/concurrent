52924626164b52a7268a6b6d77ba9b58edcb4638
tondi <mtondos2@gmail.com>
2021-06-10 00:10:11 +0200

    wip

--- src/com/agh/lab5/PK-pipe.java
+++ src/com/agh/lab5/PK-pipe.java
@@ -7,66 +7,81 @@ public class PKmon {
         System.out.println("czas: " + time);
     }
 }
+
 class Buffer {
     private int _size;
-    public Buffer(int size){
+
+    public Buffer(int size) {
         this._size = size;
     }
+
     public synchronized void put(int i) {
-        while(_buf.size() == _size){
-            try{
+        while (_buf.size() == _size) {
+            try {
                 wait();
-            }catch(InterruptedException e){}
+            } catch (InterruptedException e) {
+            }
         }
        …
     }
+
     public synchronized int get() {
-        while(_buf.size() == 0){
-            try{
+        while (_buf.size() == 0) {
+            try {
                 wait();
-            }catch(InterruptedException e){}
+            } catch (InterruptedException e) {
+            }
         }
        	…
         return value;
     }
 }
+
 class Consumer extends Thread {
     private Buffer _buf;
-    public Consumer(Buffer buffer, …){
+
+    public Consumer(Buffer buffer, …) {
         this._buf = buffer;
        …
     }
+
     public void run() {
         _buf.get();
     }
 }
-class Converter extends Thread{
+
+class Converter extends Thread {
     private Buffer previous;
     private Buffer next;
        …
-    public Converter(Buffer previous, Buffer next, …){
+
+    public Converter(Buffer previous, Buffer next, …) {
         this.previous = previous;
         this.next = next;
        …
     }
-    public void run(){
+
+    public void run() {
        …
         int tmp = previous.get();
-        try{
+        try {
             sleep(10);
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             System.out.println(e.getMessage());
         }
         next.put(tmp);
     }
 }
+
 class Producer extends Thread {
     private Buffer _buf;
     private int iter;
-    public Producer(Buffer buffer, int iterations){
+
+    public Producer(Buffer buffer, int iterations) {
         this._buf = buffer;
        …
     }
+
     public void run() {
        …
         _buf.put(i);
